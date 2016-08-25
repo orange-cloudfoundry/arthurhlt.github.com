dabee7f6bec8c09a6ca21077f6a758b0e145ecf9
ArthurH <arthur.halet@orange.com>
2016-08-25 13:00:36 +0200

    change server endpoint

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ if (/MSIE [5-9]/.test(navigator.userAgent)) {
 
 //Get Adobe Github repos & orgs
     app.factory("DatasAdobe", function ($resource) {
-        return $resource("http://org-retriever-orange.cw-lz-apps.elpaaso.net");
+        return $resource("http://org-retriever-orange-cf.cw-lz-apps.elpaaso.net");
     });
 
 //Offline backup of json
